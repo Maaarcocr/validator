91f4502e645cd9dd850a657d8ce67c511a538bcc
Marco Concetto Rudilosso <zcabmcr@ucl.ac.uk>
2016-12-28 21:25:01 +0100

    now formulas are always shorter than 50 chars

--- formulaCreator/src/main.rs
+++ formulaCreator/src/main.rs
@@ -24,7 +24,7 @@ fn negate(p: String) -> String {
     format!("-{}", p)
 }
 
-fn create_new_formula(formulas: &[String]) -> String {
+fn create_new_valid(formulas: &[String]) -> String {
     let mut rng = rand::thread_rng();
     let p = rng.choose(formulas).unwrap();
     let q = rng.choose(formulas).unwrap();
@@ -32,6 +32,7 @@ fn create_new_formula(formulas: &[String]) -> String {
     let index: usize = rng.gen();
     AXIOMS[index % 3](p,q,r)
 }
+
 
 fn write_result(formulas: Vec<String>) -> Result<(), Error> {
     let mut file = try!(File::create("input.txt"));
@@ -41,18 +42,61 @@ fn write_result(formulas: Vec<String>) -> Result<(), Error> {
     Ok(())
 }
 
+fn conj_formula(p: &String, q: &String, negated: bool) -> String {
+    if negated {
+        format!{"-({}^{})", p, q}
+    } else {
+        format!{"({}^{})",p ,q}
+    }
+}
+
+fn disj_formula(p: &String, q: &String, negated: bool) -> String {
+    if negated {
+        format!{"-({}v{})", p, q}
+    } else {
+        format!{"({}v{})",p ,q}
+    }
+
+}
+fn impl_formula(p: &String, q: &String, negated: bool) -> String {
+    if negated {
+        format!{"-({}>{})", p, q}
+    } else {
+        format!{"({}>{})",p ,q}
+    }
+}
+
+fn create_random_formulas() -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    let mut formulas: Vec<String> = vec!["p".to_string(), "q".to_string(), "r".to_string(), "-p".to_string(), "-q".to_string(), "-r".to_string()];
+    let connectives = [conj_formula, disj_formula, impl_formula];
+    for _ in 0..300 {
+        let fs = formulas.clone();
+        let p = rng.choose(&fs).unwrap();
+        let q = rng.choose(&fs).unwrap();
+        let conn = rng.choose(&connectives).unwrap();
+        let i: i32 = rng.gen();
+        let result = conn(p,q, i % 2 == 0);
+        if result.len() > 8 {continue;} else {formulas.push(result);}
+    }
+    return formulas;
+}
+
 fn main() {
-    let mut formulas: Vec<String> = vec!["p".to_string(), "q".to_string(), "r".to_string(), "(q^-r)".to_string(), "-p".to_string(), "(-pv(q>r))".to_string(), "-(q>p)".to_string()];
+    let formulas = create_random_formulas();
+    let mut valid: Vec<String> = vec![];
     for n in 0..10 {
-        let new = create_new_formula(&formulas);
+        let mut new = create_new_valid(&formulas);
+        while new.len() >= 50 {
+            new = create_new_valid(&formulas);
+        }
         if n % 2 == 0 {
-            formulas.push(negate(new));
+            valid.push(negate(new));
         } else {
-            formulas.push(new)
+            valid.push(new)
         }
     } 
-    let result: Vec<String> = formulas.drain(7..).collect(); 
-    match write_result(result) {
+    match write_result(valid) {
         Ok(_) => print!(""),
         Err(e) => panic!("ERROR {}", e)
     }
