165bab63f7f6cd2205354cf0cc8ed05baba09f2b
Marco Concetto Rudilosso <zcabmcr@ucl.ac.uk>
2017-01-05 14:58:26 +0100

    small message that informs you that the script understand your os

--- validator.sh
+++ validator.sh
@@ -1,8 +1,10 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then 
+    echo "Oh I see that you are on Linux! I'll run the correct executables"
     ./bin/formula_creator
     ./prover
     ./bin/checker
-elif [[ "$OSTYPE" == "darwin"* ]]; then 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Oh I see that you are on MacOS! I'll run the correct executables"
     ./bin/formula_creator_mac
     ./prover
     ./bin/checker_mac
