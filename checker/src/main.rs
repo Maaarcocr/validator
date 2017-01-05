b7c996d74dac5d9f106e0abf66f5f0aa78072d00
Marco Concetto Rudilosso <zcabmcr@ucl.ac.uk>
2017-01-05 13:38:01 +0100

    now checking not formula errors

--- checker/src/main.rs
+++ checker/src/main.rs
@@ -6,8 +6,9 @@ use std::fs::File;
 use regex::Regex;
 
 fn main() {
-    let satis = Regex::new(".*\x2E.*is satisfiable.").unwrap(); 
-    let notsatis = Regex::new(".*\x2E.*is not satisfiable.").unwrap();
+    let satis = Regex::new(".*\x2E.*is satisfiable.").unwrap();
+    let not_satis = Regex::new(".*\x2E.*is not satisfiable.").unwrap();
+    let not_formula = Regex::new(".*is not a formula\x2E.*").unwrap();
     let f = match File::open("output.txt") {
         Ok(file) => file,
         Err(e) => panic!("Having problem opening output.txt with error: {}", e)
@@ -17,7 +18,9 @@ fn main() {
     for line in file.lines() {
         let line_unwrapped = line.unwrap();
         let line_str = line_unwrapped.as_str();
-        if n % 2 == 0 && !notsatis.is_match(line_str) || n % 2 == 1 && !satis.is_match(line_str) {
+        if not_formula.is_match(line_str) {
+            println!("Something went wrong with formula number: {}", n);
+        } else if n % 2 == 0 && !not_satis.is_match(line_str) || n % 2 == 1 && !satis.is_match(line_str) {
             println!("Something went wrong with formula number: {}", n);
         }
         n += 1;
