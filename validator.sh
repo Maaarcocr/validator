951193d7751d3f664c443d51824aab95ede968f1
Marco Concetto Rudilosso <zcabmcr@ucl.ac.uk>
2017-01-05 14:49:38 +0100

    now macOS version exists, thank to Sadir

--- validator.sh
+++ validator.sh
@@ -1,3 +1,9 @@
-./bin/formula_creator
-./prover
-./bin/checker
+if [[ "$OSTYPE" == "linux-gnu" ]]; then 
+    ./bin/formula_creator
+    ./prover
+    ./bin/checker
+elif [[ "$OSTYPE" == "darwin"* ]]; then 
+    ./bin/formula_creator_mac
+    ./prover
+    ./bin/checker_mac
+fi
